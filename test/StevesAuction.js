6a5bb75c32d335a4d421a32ca6c444661aff2a82
Steve Kimoi <moenisteve@gmail.com>
2022-12-03 10:54:23 +0300

    test2 passing

--- test/StevesAuction.js
+++ test/StevesAuction.js
@@ -15,4 +15,15 @@ describe("Steve's Auction", function() {
        expect( await steveAuction.nft()).to.equal(addr); 
     }); 
 
+    it("Should check whether the seller is the one who just deployed the NFT", async() => {
+        const [ owner ] = await ethers.getSigners();  
+       const web3 = new Web3(Web3.givenProvider); 
+       const addr = web3.utils.toChecksumAddress("0xd9145CCE52D386f254917e481eB44e9943F39138"); 
+       const contractFactory = await ethers.getContractFactory("StevesAuction"); 
+       const steveAuction = await contractFactory.deploy(addr, 0, 1); 
+       await steveAuction.deployed(); 
+
+       expect( await steveAuction.seller()).to.equal(owner.address); 
+    }); 
+
 })
